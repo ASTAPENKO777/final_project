16e1de0a14a26c03e0b5f29eb647be65c874e9ea
fjfjvjgjrj <radcenkomisa177@gmail.com>
2025-09-15 18:44:11 +0300

    model_result

---
+++ models/result.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Table, Column, Integer, Text, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class Result(Base):
+    __tablename__ = "results"
+    id = Column(Integer, primary_key=True, index=True)
+    score = Column(Integer, nullable=False)
+    team_id = Column(Integer, ForeignKey("teams.id"))
+    tournament_id = Column(Integer, ForeignKey("tournaments.id"))
+
+    team = relationship("Team", back_populates="results")
+    tournament = relationship("Tournament", back_populates="results")
