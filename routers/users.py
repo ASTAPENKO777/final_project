76e36c379df9eedb4bf7fd1a1eb2894a48140cb8
VLUD5434 <polovlud2008@gmail.com>
2025-09-24 18:32:40 +0300

    gets router user

--- routers/users.py
+++ routers/users.py
@@ -15,3 +15,16 @@ def create_user(user: schemas.user.UserCreate, db: Session=Depends(get_db)):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+@router.get('/', response_model=list[schemas.user.UserRead])
+def all_users(db: Session = Depends(get_db)):
+    return db.query(models.user.User).all()
+
+
+@router.get('/{user_id}', response_model=schemas.user.UserRead)
+def one_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.user.User).filter(models.user.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail='User not found')
+    return user
