505d1f9866d4c64d6e1a2d2d24fe3cc9b59a7d82
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2025-09-29 18:55:34 +0300

    little fix

--- routers/tournament.py
+++ routers/tournament.py
@@ -12,11 +12,11 @@ import models.tournament
 router = APIRouter(prefix="/tournaments", tags=["Tournaments"])
 @router.post("/", response_model=schemas.tournament.TournamentRead)
 def create_tournament(tournament:schemas.tournament.Tournament, db: Session=Depends(get_db)):
-    db_tournament = models.tournament.Tournament(**tournament.dict())
+    db_tournament = models.tournament.Tournament(**tournament.model_dump())
     db.add(db_tournament)
     db.commit()
     db.refresh(db_tournament)
-    return db_tournament()
+    return db_tournament
 
 @router.get("/", response_model=list[schemas.tournament.TournamentRead])
 def get_tournaments(db: Session=Depends(get_db)):
--- schemas/tournament.py
+++ schemas/tournament.py
@@ -5,13 +5,14 @@ from datetime import datetime
 
 class Tournament(BaseModel):
     title: str = Field(...)
-    discription: Optional[str] = None
+    description: Optional[str] = None
     start_date: datetime
     end_date: Optional[datetime] = None
 
 
 class TournamentRead(BaseModel):
     id: int
+    title: str
 
     class Config:
         from_attributes = True
