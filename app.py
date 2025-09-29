ff4e38af4a304073adec0733d23bb08425ea6daa
LAZE4ER <89158736+LAZE4ER@users.noreply.github.com>
2025-09-29 19:30:18 +0300

    chat

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from routers import users, team, result, tournament
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
-
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def home():
     return {"message": "Final project"}
+@app.get("/chat", response_class=HTMLResponse)
+async def chat():
+    return templates.TemplateResponse("chat.html", {"request": {}})
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+
+
 
 
 app.include_router(users.router)
