79a85f16638e412859c60251b999f6c6fb677e20
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2025-10-01 19:32:14 +0300

    readme upd

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# final_project
+# Tournament FastAPI
+
+
+## Features
+- Users: create, read
+- Teams: create, read
+- Tournament: create, read
+- Result: create, read, read best
+- WebSocket Chat
+
+## Tech stack
+- FastAPI
+- SQLALChemy ORM
+- Pydantic
+- SQLite
+
+## Install
+```
+pip install -r requirements.txt
+```
+
+Create DB example - env_example
+
+## Run 
+```
+uvicorn app:app --reload
+```
--- routers/result.py
+++ routers/result.py
@@ -4,10 +4,7 @@ from sqlalchemy.orm import Session
 from create_db import get_db
 import schemas.result
 import models.result
-import schemas.team
-import models.team
-import schemas.tournament
-import models.tournament
+
 router = APIRouter(prefix='/result', tags=['Result'])
 
 @router.post('/', response_model=schemas.result.ResultRead)
@@ -22,22 +19,13 @@ def create_result(result: schemas.result.ResultCreate, db: Session = Depends(get
 def read_results(db: Session = Depends(get_db)):
     return db.query(models.result.Result).all()
 
-@router.get('/best', response_model=schemas.result.ResultWithDetail)
+@router.get('/best', response_model=schemas.result.ResultRead)
 def best_result(db: Session = Depends(get_db)):
     best = (
         db.query(models.result.Result)
-        .join(models.team.Team, models.team.Team.id == models.result.Result.team_id)
-        .join(models.tournament.Tournament, models.tournament.Tournament.id == models.result.Result.tournament_id)
         .order_by(models.result.Result.score.desc())
         .first()
-        )
+    )
     if not best:
         raise HTTPException(status_code=404, detail='not found')
-    return {
-        'id': best.id,
-        'score': best.score,
-        'team id': best.team_id,
-        'tournament id': best.tournament_id,
-        'team name': best.team.name,
-        'tournament title': best.tournament.title
-    }
+    return best
